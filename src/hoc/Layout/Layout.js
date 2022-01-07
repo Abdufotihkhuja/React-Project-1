071379a3b644f342f823c3d556a9c02f06510d8b
abdufotihkhuja <formymac15@gmail.com>
2022-01-08 00:29:14 +0500

    first commit

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Layout extends Component{
+    render(){
+        return (
+            <div>
+
+
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Layout
